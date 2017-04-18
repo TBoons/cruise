7b7c8fb79b024136413625fb1ba1081a13f02f9f
Tim Boonstra <boonstra@gmail.com>
2017-04-17 22:01:31 -0500

    Working ignition and cruise buttons

--- cruiseControl.java
+++ cruiseControl.java
@@ -19,8 +19,14 @@ public class cruiseControl extends JFrame{
     private JTextField item2;
     private JTextField currentSpeed;
     private JLabel currentSpeedLabel;
+    
     private JButton ignitionButton;
-    private JLabel engineStatus;
+    public JLabel engineStatus;
+    
+    private JButton cruiseOnOffButton;
+    public JLabel cruiseOnOffStatus;
+    
+    private systemStatus ss = new systemStatus();
     
     public cruiseControl(){
         super("Java Cruise Control");
@@ -43,7 +49,7 @@ public class cruiseControl extends JFrame{
         c.gridx = 1;
         c.gridy = 0;
         
-        currentSpeed = new JTextField(String.valueOf(ss.getCurrentSpeed()),5);
+        currentSpeed = new JTextField( String.valueOf(ss.getCurrentSpeed()), 5);
         currentSpeed.setEditable(false);
         add(currentSpeed,c);
         
@@ -52,40 +58,52 @@ public class cruiseControl extends JFrame{
         c.gridy = 1;
         add(ignitionButton,c);
         
-        engineStatus = new JLabel("off");
+        engineStatus = new JLabel( "off" ); //start off
         engineStatus.setForeground(Color.red);
         c.gridx = 1;
         c.gridy = 1;
         add(engineStatus,c);
+        
+        cruiseOnOffButton = new JButton("Cruise");
+        c.gridx = 0;
+        c.gridy = 2;
+        add(cruiseOnOffButton,c);
+        
+        cruiseOnOffStatus = new JLabel( "off" ); //start off
+        cruiseOnOffStatus.setForeground(Color.red);
+        c.gridx = 1;
+        c.gridy = 2;
+        add(cruiseOnOffStatus,c);
         
         thehandler handler = new thehandler();
         
         //item1.addActionListener(handler);
         //item2.addActionListener(handler);
         currentSpeed.addActionListener(handler);
+        ignitionButton.addActionListener(handler);
+        cruiseOnOffButton.addActionListener(handler);
         
     }
     
     private class thehandler implements ActionListener{
         public void actionPerformed(ActionEvent event){
-            String string = "";
             
-            if(event.getSource()==item1){
-                //System.out.println(item1.getText());
-                //string=String.format("field 1: %s", event.getActionCommand());
-            } else if (event.getSource()==item2){
-                //string=String.format("field 2: %s", event.getActionCommand());
+            systemStatus ss = new systemStatus();
+           
+            if(event.getSource()==cruiseOnOffButton){
+                cruiseOnOff( false );
+            } else if (event.getSource()==ignitionButton){
+                ignition();
             } else if (event.getSource()==currentSpeed){
-                string=String.format("field 3: %s", event.getActionCommand());
+                //string=String.format("field 3: %s", event.getActionCommand());
             } 
-            
-            JOptionPane.showMessageDialog(null, string);
+            //JOptionPane.showMessageDialog(null, string);
         }
     }
     
-    private class systemStatus{
-        private String ignitionStatus;
-        private String cruiseControlStatus;
+    public class systemStatus{
+        private String ignitionStatus = "off";
+        private String cruiseControlStatus = "off";
         private int currentSpeed = 60;
         private int cruiseControlSpeedSetting;
         
@@ -105,6 +123,42 @@ public class cruiseControl extends JFrame{
             return currentSpeed;
         }
         
+        //Cruise Control Button
+        public void setCruiseOnOffStatus(String i){
+            cruiseControlStatus = i;
+        }
+        public String getCruiseOnOffStatus(){
+            return cruiseControlStatus;
+        }
+    }
+   
+    public void ignition(){
+        if( ss.getIgnitionStatus() == "off" ){
+            //Turn on
+            ss.setIgnitionStatus("on");
+            engineStatus.setText( "on" );
+            engineStatus.setForeground(Color.green);
+        } else {
+            //Turn off
+            ss.setIgnitionStatus("off");
+            engineStatus.setText( "off" );
+            engineStatus.setForeground(Color.red);
+            cruiseOnOff( true );
+        }
+    }
+    
+    public void cruiseOnOff(boolean overrideStatus){
+        if( ss.getCruiseOnOffStatus() == "off" && ss.getIgnitionStatus() == "on" && !overrideStatus ){ //only turn on when engine is on
+            //Turn on
+            ss.setCruiseOnOffStatus("on");
+            cruiseOnOffStatus.setText( "on" );
+            cruiseOnOffStatus.setForeground(Color.green);
+        } else {
+            //Turn off
+            ss.setCruiseOnOffStatus("off");
+            cruiseOnOffStatus.setText( "off" );
+            cruiseOnOffStatus.setForeground(Color.red);
+        }
     }
     
 }
